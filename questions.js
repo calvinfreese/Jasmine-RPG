5d87a954748a15039cb86a3b175e6ae63cc04df8
William Sheehan <66801979+WilliamJohnSheehan@users.noreply.github.com>
2020-08-04 19:05:12 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,27 @@
-# homework4
+var questions = [
+  ["How many days are in a year?", "365", "300", "355", "360", "A"],
+  [
+    "In what direction does the sun set?",
+    "South",
+    "East",
+    "North",
+    "West",
+    "D",
+  ],
+  [
+    "Who discovered electricity?",
+    "Thomas Edison",
+    "Neil Gaiman",
+    "Benjamin Franklin",
+    "Boris Yeltsin",
+    "C",
+  ],
+  [
+    "How many years does it take Halley's Comet to return to Earth?",
+    "20",
+    "75",
+    "100",
+    "5",
+    "B",
+  ],
+];
