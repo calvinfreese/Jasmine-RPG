8a462374476915fa85c8c19e61c137449a309591
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-10-22 23:08:42 -0500

    added randomize function

--- public/js/missions.js
+++ public/js/missions.js
@@ -1,8 +1,11 @@
+let arr = ['alpha', 'beta', 'charlie', 'delta', 'edward'];
+
 function startAdventure() {
     console.log("hi");
     $(".mission-box").hide(1000,"swing",function(){
         console.log('hidden');
         $(".mission-animation").show();
+        
     });
     
 }
@@ -12,8 +15,26 @@ function resetAdventure() {
     $(".mission-animation").hide(400, "swing", function(){
         console.log("show mission select");
         $(".mission-box").show();
+        animate();
     })
 }
 
+
+
+
+function randomizeMissions() {
+    
+    
+    for (let i = 0; i < arr.length; i++) {
+        
+    $('.1mission').text(arr[Math.floor(Math.random() * arr.length)]);
+    $('.2mission').text(arr[Math.floor(Math.random() * arr.length)]);
+    $('.3mission').text(arr[Math.floor(Math.random() * arr.length)]);
+
+    }
+}
+
+
+randomizeMissions();
 $(".hide-mission").on("click", startAdventure);
 $(".show-mission").on("click", resetAdventure);
