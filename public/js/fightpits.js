cc9f9adbc663328757b512926bb5174ae03e7ccb
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-10-25 22:09:31 -0500

    removed progress bars -- added fight sequence and HP updates. XP added gets added to str and hp in database

--- public/js/fightpits.js
+++ public/js/fightpits.js
@@ -1,5 +1,6 @@
+// calls/requests to database
 let API = {
-    getCharacter: function(selected, id) {
+    getOne: function(selected, id) {
         
         return $.ajax({
             type: "GET",
@@ -7,41 +8,47 @@ let API = {
         });
     },
 
-    enemyToDatabase: function(newEnemy) {
-        console.log('heeyyy!');
-        console.log(newEnemy);
+    putOne: function(payload,type) {
         return $.ajax({
-            headers: {
-                "Content-Type": "application/json"
-            },
-            type: "POST",
-            url: "/api/enemies",
-            data: JSON.stringify(newEnemy)
+            method: "PUT",
+            url: "/api/"+ type,
+            data: payload
         });
     }
 }
 
+
+// class setup
 class Entity {
-    constructor(name, race, strength, health) {
+    constructor(id, name, race, strength, health) {
+        this.id = id;
         this.name=name;
         this.race=race;
         this.strength=strength;
         this.health=health;
     }
 
+    
     attack(opponent){
         opponent.health -= this.strength;
     }
 
     isAlive(){
         if(this.health <= 0) {
-            console.log(`${this.name} has died`);
+            alert(`${this.name} has died`);
             return false; 
         } 
         return true;
     }
 }
 
+
+// globals
+let characterEntity;
+let enemyEntity;
+
+
+// onclick of 'next' button, show next input
 function hideInput1() {
     $(".input1").hide(400, "swing", function(){
        
@@ -50,37 +57,115 @@ function hideInput1() {
     });
 }
 
+// save values of character/enemy selection
 function saveSelectValues(event) {
+
     event.preventDefault();
     let characterID = $("#char-select").val().trim();
     let enemyID = $("#enemy-select").val().trim();
+
+
     $(".mission-box").hide(400, "swing", function(){
         $(".fight").show();
         $(".character-name").attr("data-id", characterID);
         $(".enemy-name").attr("data-id", enemyID);
-        API.getCharacter("enemies", enemyID).then(function(enemy){
-            $(".enemy-name").text(enemy.name)
-            console.log(enemy);
+
+        API.getOne("enemies", enemyID).then(function(enemy){
+            $(".enemy-name").text(enemy.name);
+            enemyEntity = new Entity(enemy.id, enemy.name, enemy.race, enemy.strength, enemy.health);
+            $(".enemy-full").text(enemyEntity.health);
+            
+            $(".enemy-health").text(enemyEntity.health);
+
         });
-        API.getCharacter("characters", characterID).then(function(character){
-            $(".character-name").text(character.name)
-            console.log(character);
+
+        API.getOne("characters", characterID).then(function(character){
+            $(".character-name").text(character.name);
+            characterEntity = new Entity(character.id, character.name, character.race, character.strength, character.health);
+            $(".character-full").text(characterEntity.health);
+
+            $(".character-health").text(characterEntity.health);
+         
         });
+    });
+}
+
+
+function attackEnemy(char, enemy){
+    char = characterEntity;
+    enemy = enemyEntity;
+
+    if(enemy.isAlive() || enemy.health > 0){
+        char.attack(enemy)
+        enemy.isAlive();
+        if(!enemy.isAlive()) {
+            endMatch();
+            updateDatabaseWithExp(char, enemy);
+        } else {
+            updateHealthBar(char, enemy);
+            enemyAttack(char, enemy);
+        }        
+    } else if (enemy.health <= 0) {
+        endMatch();
+        updateDatabaseWithExp(char, enemy);
+    } 
+    
+}
+
+function endMatch() {
+    $(".return-adv").show();
+    $(".attack").hide();
+    $(".flee").hide();
+}
+
+function enemyAttack(char, enemy) {
+    if(char.isAlive() || char.health > 0) {
+        enemy.attack(char);
+        char.isAlive();
+        if(!char.isAlive()) {
+            endMatch();
+        } else {
+            updateHealthBar(char, enemy);
+        }     
+    } else {
+        endMatch();
     }
-    );
 }
 
-function classInstantiation(entity) {
-    let newEntity = new Entity(entity.name, entity.race, entity.strength, entity.health);
-     
+function updateDatabaseWithExp(char, enemy) {
+    strXP = enemy.strength /2;
+    healthXP = enemy.strength /2;
+
+    $(".strXP").text(strXP);
+    $(".healthXP").text(healthXP);
+    console.log("str exp gained", strXP);
+    console.log("health exp gained", healthXP);
+    addXP(char, strXP, healthXP);
 }
 
-function attackEnemy(){
-    alert("You're swinging at the enemy! Woah!");
+function updateHealthBar(char, enemy) {
+    $(".character-health").text(char.health);
+    $(".enemy-health").text(enemy.health);
 }
+
+
+
+function addXP(char, strXP, healthXP) {
+    char.strength += strXP;
+    char.health += healthXP;
+       
+    API.putOne(char, "characters").then(function(posted){
+        console.log("EXP has been granted!");
+    });
+
+    $(".return-adv").show();
+
+}
+
 
 function fleeFightpits(){
-    alert("You're Fleeing!");
+    alert("You're Fleeing! No XP gained.");
+    window.location.replace("/adventures");
 }
 
 $(".next").on("click", hideInput1);
